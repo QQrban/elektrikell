bcb499f090cad2021b38449b2bfff6fae9672298
Kurban Ramazanov <kurban.ramazanovv@gmail.com>
2023-02-13 15:21:54 +0200

    fafa

--- src/PriceHeader.js
+++ src/PriceHeader.js
@@ -1,12 +1,15 @@
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
+import HoursButtonsComponent from './HoursButtons';
 
 function PriceHeader() {
   return (
-      <Row className='mb-2'>
+      <Row className='mb-2 d-flex align-items-center'>
         <Col>1 of 3</Col>
-        <Col>2 of 3</Col>
-        <Col>3 of 3</Col>
+        <Col className='text-center'>
+          <HoursButtonsComponent/>
+        </Col>
+        <Col className='text-end'>3 of 3</Col>
       </Row>
   );
 }
