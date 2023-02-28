681b618f12c8ba46ec630485d34434a4c2266cb0
Kurban Ramazanov <kurban.ramazanovv@gmail.com>
2023-02-28 23:07:29 +0200

    parim aeg footer low price added

--- src/Footer/FooterLowPrice.js
+++ src/Footer/FooterLowPrice.js
@@ -1,19 +1,23 @@
 import Container from 'react-bootstrap/Container';
 import Durations from './Durations';
 import Countdown from 'react-countdown';
+import moment from 'moment';
 
 function FooterLowPrice(props) {
+    const timeFrom = moment(props.lowPriceTimestamp * 1000).format('HH:mm');
+    const seconds = props.hourRange * 3600;
+    const timeTo = moment((props.lowPriceTimestamp + seconds) * 1000).format('HH:mm');
     return (
         <Container className="text-center mt-3 fw-light">
             <div className='fs-2 mb-1'>Tahan tarbida</div>
             <div className='mb-2'>
                 <Durations {...props} />
             </div>
-            <div className='mb-1 fs-5'>Parim aeg selleks on <span className='fw-normal'>0:00st 1:00ni, </span>milleni on jäänud</div>
+            <div className='mb-1 fs-5'>Parim aeg selleks on <span className='fw-normal'>{timeFrom}st {timeTo}ni </span>milleni on jäänud</div>
             <div className='mb-1 fs-1 fw-bold'>
                 {props.lowPriceTimestamp && <Countdown date={props.lowPriceTimestamp * 1000} />}
             </div>
-            <div className='fs-5'>Siis on kilovatt-tunni hind <span className='text-success fw-normal'>7.80</span> senti, mis on <span className='fw-bold'>53%</span> odavam kui praegu</div>
+            <div className='fs-5'><a href="https://mkm.ee/energeetika-ja-maavarad/energiatohusus/saastlik-energiatarbimine">Loe lähemalt säästliku energiatarbimist</a></div>
         </Container>
     );
 }
